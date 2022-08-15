f75e478caabc4a07e6f4e7f21ae4839ce093a4ca
Christina Hong <hongchristinaaa@gmail.com>
2022-08-14 23:45:06 -0700

    Added routes and get requests. But GET request for note page is not working properly. It says /notes can not be found

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+const diagnosticsRouter = require('./diagnostics');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+app.use('/diagnostics', diagnosticsRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -6,3 +6,28 @@ const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for Homepage
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for Note Page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.json(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/404.html')));
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
